604d22f440b9fc91cdbf96f5c37433a3324a49df
Nga Rampling <ngarampling@192-168-1-19.tpgi.com.au>
2022-07-21 16:46:20 +0930

    populate routes.rb file for DRUD functionality and initiated client.http for request testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   resources :wine_listings
+
+  scope '/api' do
+    get '/wine_listings', to: 'wine_listings#index'
+    post '/wine_listings', to: 'wine_listings#create'
+    get '/wine_listings/:id', to: 'wine_listings#show'
+    put '/wine_listings/:id', to: 'wine_listings#update'
+    delete '/wine_listings/:id', to: 'wine_listings#destroy'
+    
+    # scope '/auth' do
+    #   post '/sign_up', to: 'users#create'
+    #   post '/sign_in', to: 'users#sign_in'
+    # end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
