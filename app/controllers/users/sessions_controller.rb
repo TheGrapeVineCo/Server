46ae7d885ae4b7d6c77fc9e9db16ed7e79a0b382
Nga Rampling <ngarampling@192-168-1-19.tpgi.com.au>
2022-07-24 01:20:40 +0930

    fix sign_out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,23 +1,23 @@
 class Users::SessionsController < Devise::SessionsController
     respond_to :json
   
-    # private
+    private
   
-    # def respond_with(resource, _opts = {})
-    #   render json: { message: 'You are logged in.' }, status: :ok
-    # end
+    def respond_with(resource, _opts = {})
+      render json: { message: 'You are logged in.' }, status: :ok
+    end
   
-    # def respond_to_on_destroy
-    #   log_out_success && return if current_user
+    def respond_to_on_destroy
+      log_out_success && return if current_user
   
-    #   log_out_failure
-    # end
+      log_out_failure
+    end
   
-    # def log_out_success
-    #   render json: { message: "You are logged out." }, status: :ok
-    # end
+    def log_out_success
+      render json: { message: "You are logged out." }, status: :ok
+    end
   
-    # def log_out_failure
-    #   render json: { message: "Hmm nothing happened."}, status: :unauthorized
-    # end
+    def log_out_failure
+      render json: { message: "Hmm nothing happened."}, status: :unauthorized
+    end
 end
