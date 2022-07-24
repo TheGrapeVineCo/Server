2906f27c3ef4ae455b32cef34acbf0de5cac2e34
Nga Rampling <ngarampling@192-168-1-11.tpgi.com.au>
2022-07-24 23:46:08 +0930

    adds method to create admin role account in seeds file

--- db/migrate/20220724064140_add_admin_to_users.rb
+++ db/migrate/20220724064140_add_admin_to_users.rb
@@ -2,4 +2,8 @@ class AddAdminToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :admin, :boolean, default: false
   end
+
+  def self.down
+    remove_column :users, :admin
+  end
 end
