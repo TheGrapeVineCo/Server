2926e3a35306cd4f047d1b2fda0d941a2a63f019
Nga Rampling <ngarampling@192-168-1-11.tpgi.com.au>
2022-07-31 14:14:04 +0930

    small cleanup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,11 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # POST /comments
   # def create
   #   @user = User.create(sign_up_params);
-  #   # if @comment.save
-  #   #   render json: @comment, status: :created, location: @comment
-  #   # else
-  #   #   render json: @comment.errors, status: :unprocessable_entity
-  #   # end
   # end
 
     private
