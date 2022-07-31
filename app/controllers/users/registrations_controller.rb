2fd5a00d1aa53608d29f0b854157189d909cd076
Nga Rampling <ngarampling@192-168-1-11.tpgi.com.au>
2022-07-31 14:04:15 +0930

    adds params to include username when sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     respond_to :json
 
-    
-  
+    # POST /comments
+  # def create
+  #   @user = User.create(sign_up_params);
+  #   # if @comment.save
+  #   #   render json: @comment, status: :created, location: @comment
+  #   # else
+  #   #   render json: @comment.errors, status: :unprocessable_entity
+  #   # end
+  # end
+
     private
   
     def respond_with(resource, _opts = {})
@@ -18,4 +26,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def register_failed
       render json: { message: "Something went wrong." }
     end
+
+    # Notice the name of the method
+    # def sign_up_params
+    #   params.require(:user).permit(:username, :email, :password)
+    # end
+
 end
